7e631aa226261e912da11d6899da4ee4957e1923
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2023-04-16 16:50:06 +0100

    Add DB context to project

---
+++ DisqussTopics/Data/DTContext.cs
@@ -0,0 +1,32 @@
+﻿using DisqussTopics.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisqussTopics.Data
+{
+    public class DTContext : DbContext
+    {
+        public DTContext(DbContextOptions<DTContext> options) : base(options) { }
+
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Rule> Rules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Topic>()
+                .HasMany(t => t.Posts)
+                .WithOne(p => p.Topic);
+
+            modelBuilder.Entity<Topic>()
+                .HasMany(t => t.Rules)
+                .WithOne(r => r.Topic);
+
+            modelBuilder.Entity<Post>()
+                .HasMany(t => t.Comments)
+                .WithOne(p => p.Post);
+        }
+    }
+}
